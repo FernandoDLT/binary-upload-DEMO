4a744f44a64fee8af5344b7cc64a60b47ae7f20c
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-08-22 14:57:53 -0500

    Updated cloudinary variables

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -3,32 +3,9 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config({ path: "./config/.env" });
 
 cloudinary.config({ 
-  cloud_name: 'one100devs', 
-  api_key: '436478914517416',
-  // api_secret: process.env.API_SECRET,
-  api_secret: 'UOId4CmTCIqoJSkvnEbbsuoFbmw' 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
-
-// cloudinary.config({ 
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-//   api_key: process.env.CLOUDINARY_API_KEY, 
-//   api_secret: process.env.CLOUDINARY_API_SECRET 
-// });
-
-
-
-// cloudinary.config({ 
-//   cloud_name: 'one100devs', 
-//   api_key: '674222146271512', 
-//   api_secret: '***************************' 
-// });
-
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.API_KEY,
-//   api_secret: process.env.API_SECRET,
-// });
 
 module.exports = cloudinary;
